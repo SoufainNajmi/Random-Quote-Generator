f86a4c5d4b561801d0a818a67a9fe3cb20bb0173
SoufainNajmi <soufainnajmi@gmail.com>
2025-08-02 17:38:13 +0100

    version1

---
+++ script.js
@@ -0,0 +1,19 @@
+ const citation = document.getElementById('citation');
+        const autor = document.getElementById('autor');
+        const btn = document.getElementById('btn');
+        const citations = [
+            {text: "Le succès n’est pas la clé du bonheur. Le bonheur est la clé du succès.", author: "Albert Schweitzer"},
+            {text: "Ils ne savaient pas que c’était impossible, alors ils l’ont fait.", author: "Mark Twain"},
+            {text: "La meilleure façon de prédire l’avenir, c’est de le créer.", author: "Peter Drucker"},
+            {text: "Ne rêve pas ta vie, vis tes rêves.", author: "Anonyme"},
+            {text: "Ce qui ne nous tue pas nous rend plus fort.", author: "Friedrich Nietzsche"},
+            {text: "Fais de ta vie un rêve, et d’un rêve, une réalité.", author: "Antoine de Saint-Exupéry"}
+        ];
+     btn.addEventListener('click', function() {
+            const randomIndex = Math.floor(Math.random() * citations.length);
+            citation.textContent = citations[randomIndex].text;
+            autor.textContent = `- ${citations[randomIndex].author} -`;
+        });
+
+        // Initial call to display a random quote on page load
+        btn.click();
